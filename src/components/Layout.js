99d2e622eb49f0a9e6024cc9d7db18e5a9cf0a8e
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2023-07-05 21:32:03 -0400

    init

---
+++ src/components/Layout.js
@@ -0,0 +1,34 @@
+import logo from "../images/Jasmyne Jean-Remy Logo.png"
+import "./layout.css"
+function Layout({children}){
+    return (
+        <div className="w-screen h-screen flex flex-col">
+            <header className="w-full grid grid-cols-2 items-center drop-shadow-sm">
+
+                <img src={logo} className="w-20 ml-2"/>
+                <div className="flex w-1/2 justify-between justify-self-end mr-3" id="nav-items">
+                    <div className="nav-item">
+                        <p>Resume</p>
+                    </div>
+                    <div className="nav-item">
+                        <p>Skills</p>
+                    </div>
+                    <div className="nav-item">
+                        <p>Services</p>
+                    </div>
+                    <div className="nav-item">
+                        <p>Contact</p>
+                    </div>
+                </div>
+            </header>
+            <main className="flex-grow">
+                {children}
+            </main>
+            <footer className="text-center p-2">
+                <p>This Website was made with React JS, Tailwind CSS and &#128150;</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout;
