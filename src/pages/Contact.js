63b13ee23e36f7b091513a61a763fe72183ba70c
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2023-07-06 22:58:49 -0400

    contact and services

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,11 @@ import Layout from "../components/Layout";
 function Contact(){
     return (
         <Layout activePage={"contact"}>
-            <h1>Contact</h1>
+            <div className="flex flex-col items-center">
+                <h1 className="text-5xl border-b-4">Contact</h1>
+                <p className="text-3xl font-extralight">Email: jasmyne@jasmynejr.com</p>
+            </div>
+
         </Layout>
     )
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,7 +3,11 @@ import Layout from "../components/Layout";
 function Services(){
     return (
         <Layout activePage="services">
-            <h1>Services</h1>
+            <div>
+                <h1 className="text-5xl">Services</h1>
+                <p className="text-3xl font-extralight">Coming Soon</p>
+            </div>
+
         </Layout>
     )
 }
